2d7d97f4344d46f9b1cbbfab35a9762fdfc43b1e
Nyokana <nyokanaanele@gmail.com>
2022-06-19 17:38:27 +0200

    added hello world

--- src/stPoint.java
+++ src/stPoint.java
@@ -4,6 +4,9 @@ public class stPoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World");
+		System.out.print("Hello World");
+		System.out.print("Hello World");
+		System.out.print("Hello World");
 
 	}
 
